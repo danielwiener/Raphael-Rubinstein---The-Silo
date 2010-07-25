6c5ac93fe0d4e0a3ed0133f04a87a9feafb4867a
danielwiener <daniel@danielwiener.com>
2010-07-25 12:25:07 -0400

    got rid of the silo in the bar across the top

--- header.php
+++ header.php
@@ -87,6 +87,7 @@ jQuery(document).ready(function($)  {
 		</div>
 		</div>
 		<div class="tab">
+
 			<div id="toggle" class="slide">
 				<a id="open" class="open" title="Open panel">Open &nbsp;<span class="arrow">&darr;</span></a> <a id="close" style="display: none;" class="close" title="Close panel">Close &nbsp;<span class="arrow">&uarr;</span></a>
 
@@ -95,29 +96,6 @@ jQuery(document).ready(function($)  {
 	</div>
 <div id="outer_wrapper">
 <div id="wrapper" class="hfeed">
-	<div id="header">
-		<div id="masthead">
-			<div id="branding" role="banner">
-				<?php $heading_tag = ( is_home() || is_front_page() ) ? 'h1' : 'div'; ?>
-				<<?php echo $heading_tag; ?> id="site-title">
-					<!--
-<span>
-						<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-					</span>
--->
-				</<?php echo $heading_tag; ?>>
-				<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-			</div><!-- #branding -->
-
-		<!--
-	<div id="access" role="navigation">
-			  <?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff */ ?>
-				<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentyten' ); ?>"><?php _e( 'Skip to content', 'twentyten' ); ?></a></div>
-				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
-				<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
-			</div><!-- #access -->
--->
-		</div><!-- #masthead -->
-	</div><!-- #header -->
+	
 
 	<div id="main">
